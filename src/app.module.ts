5729cf4a5707d158582dcabec5902cf4e3066aa6
mahdiazizzadeh <yasermahdiazizzadeh@gmail.com>
2023-06-19 16:30:41 +0330

    graphql error result format changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { MongooseModule } from "@nestjs/mongoose";
+import { GraphQLError, GraphQLFormattedError } from "graphql";
 import { AuthModule } from "./components/auth/auth.module";
 import { StoryModule } from "./components/story/story.module";
 
@@ -14,6 +15,12 @@ import { StoryModule } from "./components/story/story.module";
       typePaths: ["src/components/**/*.graphql"],
       sortSchema: true,
       context: ({ req }) => ({ req }),
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          errors: error.extensions.originalError,
+        } as any;
+        return graphQLFormattedError;
+      },
     }),
     AuthModule,
     StoryModule,
